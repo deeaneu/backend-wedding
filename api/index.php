42ae50f57b793c2fac5cf53bf2be202b70043d62
hasyim404 <muhaisim7@gmail.com>
2023-12-22 19:15:20 +0700

    test cors

--- api/index.php
+++ api/index.php
@@ -1,5 +1,9 @@
 <?php
 
+header('Access-Control-Allow-Origin: https://wedding-fitria-jaelani.vercel.app');
+header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
+header('Access-Control-Allow-Headers: Content-Type');
+
 /**
  * Redirect request to public
  * 
