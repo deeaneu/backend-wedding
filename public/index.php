6d8ed49d2b1826faed1baa8efa895a470416a0da
hasyim404 <muhaisim7@gmail.com>
2023-12-22 19:35:08 +0700

    htaccess

--- public/index.php
+++ public/index.php
@@ -1,11 +1,5 @@
 <?php
 
-header('Access-Control-Allow-Origin: https://wedding-fitria-jaelani.vercel.app');
-header('Access-Control-Allow-Origin: http://localhost:5500');
-header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
-header('Access-Control-Allow-Headers: Content-Type');
-
-
 /**
  * Import semua class yang digunakan dalam framework ini.
  * Tenang, ini telah dilakukan secara otomatis oleh composer.
