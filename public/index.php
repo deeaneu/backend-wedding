beb9f1e2988893c5a4135f33c8d8d349d1b7c1d6
hasyim404 <muhaisim7@gmail.com>
2023-12-22 19:24:29 +0700

    testing 2 cors

--- public/index.php
+++ public/index.php
@@ -1,5 +1,30 @@
 <?php
 
+header('Access-Control-Allow-Origin: https://wedding-fitria-jaelani.vercel.app');
+header('Access-Control-Allow-Origin: http://localhost:5500');
+header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
+header('Access-Control-Allow-Headers: Content-Type');
+
+// Periksa metode permintaan, jika OPTIONS, kirim respons kosong
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    header('HTTP/1.1 200 OK');
+    exit();
+}
+
+// Proses permintaan sesuai kebutuhan aplikasi Anda
+if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+    // Handle permintaan GET
+    echo json_encode(['message' => 'Permintaan GET berhasil']);
+} elseif ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    // Handle permintaan POST
+    $data = json_decode(file_get_contents('php://input'), true);
+    echo json_encode(['message' => 'Permintaan POST berhasil', 'data' => $data]);
+} else {
+    // Metode permintaan tidak didukung
+    header('HTTP/1.1 405 Method Not Allowed');
+    echo json_encode(['error' => 'Metode permintaan tidak didukung']);
+}
+
 /**
  * Import semua class yang digunakan dalam framework ini.
  * Tenang, ini telah dilakukan secara otomatis oleh composer.
